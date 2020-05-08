1ee68a914a219dfbbf05dfb98b9a8f118d538438
Alfred-Lau <jian.liu@icloud.com>
2020-05-08 19:25:44 +0800

    feat: adjust about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="about">
     about
+    <div class="about-container">
+      <iframe
+        src="http://www.baidu.com"
+        frameborder="0"
+        title="Inline Frame Example"
+        width="100%"
+        height="600"
+      >
+      </iframe>
+    </div>
   </div>
 </template>
 
@@ -8,5 +18,5 @@
 export default {
   name: 'About'
 
-}
+};
 </script>
