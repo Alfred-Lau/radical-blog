e259d7667672216041a63bb3eedbb273d5e38fa4
Alfred-Lau <jian.liu@icloud.com>
2020-05-10 16:15:25 +0800

    fix: close animation

--- src/base/Slider/slider.vue
+++ src/base/Slider/slider.vue
@@ -1,102 +1,98 @@
 <template>
   <div class="container">
     <div class="swiper-box">
-        <swiper :options="swiperOptions">
-          <swiper-slide v-for="item in swiperData" :key="item.id">
-            <a href=""
-              ><img :src="`/imgs/3.webp`" alt=""
-            /></a>
-          </swiper-slide>
-          <!-- 如果需要导航按钮 -->
-          <div class="swiper-button-prev" slot="button-prev"></div>
-          <div class="swiper-button-next" slot="button-next"></div>
+      <swiper :options="swiperOptions">
+        <swiper-slide v-for="item in swiperData" :key="item.id">
+          <a href=""><img :src="`/imgs/3.webp`" alt=""/></a>
+        </swiper-slide>
+        <!-- 如果需要导航按钮 -->
+        <div class="swiper-button-prev" slot="button-prev"></div>
+        <div class="swiper-button-next" slot="button-next"></div>
 
-          <!-- 分页器 -->
-          <div class="swiper-pagination" slot="pagination"></div>
-        </swiper>
-      </div>
+        <!-- 分页器 -->
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Swiper, SwiperSlide } from 'vue-awesome-swiper';
+import { Swiper, SwiperSlide } from "vue-awesome-swiper";
 
-import 'swiper/css/swiper.css';
+import "swiper/css/swiper.css";
 
 export default {
-  name: 'Slider',
+  name: "Slider",
   components: {
     Swiper,
     SwiperSlide
   },
-  data () {
+  data() {
     return {
       swiperOptions: {
         /* basic */
         autoplay: true,
         loop: true,
-        effect: 'cube',
-        cubeEffect: {
-          shadow: false
-        },
         /* 分页 */
         pagination: {
-          el: '.swiper-pagination',
+          el: ".swiper-pagination",
           clickable: true
         },
         /* 前进后退 */
         navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev'
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev"
         }
       },
       swiperData: [
         {
           id: 1,
-          link: ''
+          link: ""
         },
         {
           id: 2,
-          link: ''
-        }, {
+          link: ""
+        },
+        {
           id: 3,
-          link: ''
-        }, {
+          link: ""
+        },
+        {
           id: 4,
-          link: ''
-        }, {
+          link: ""
+        },
+        {
           id: 5,
-          link: ''
+          link: ""
         }
       ]
     };
   },
-  methods: {
-  },
+  methods: {},
   props: {
     imgList: {
       type: Array,
-      default () {
+      default() {
         return [
           {
             id: 1,
-            title: 'bgcolor'
+            title: "bgcolor"
           },
           {
             id: 2,
-            title: 'bgcolor'
+            title: "bgcolor"
           },
           {
             id: 3,
-            title: 'bgcolor'
+            title: "bgcolor"
           },
           {
             id: 4,
-            title: 'ds'
+            title: "ds"
           },
           {
             id: 5,
-            title: 'ds'
+            title: "ds"
           }
         ];
       }
@@ -106,25 +102,24 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .container {
-    margin: auto;
-    width: 1000px;
-    height: 500px;
-    overflow: hidden;
+.container {
+  margin: auto;
+  width: 1000px;
+  height: 500px;
+  overflow: hidden;
 
-    // @include middle();
-    .swiper-box {
-      position: relative;
+  // @include middle();
+  .swiper-box {
+    position: relative;
+    width: 100%;
+    img {
       width: 100%;
-      img {
-        width: 100%;
-      }
-
-      .swiper-button-prev,
-      .swiper-button-next {
-        transform: translateY(-95px);
-      }
     }
 
+    .swiper-button-prev,
+    .swiper-button-next {
+      transform: translateY(-95px);
+    }
   }
+}
 </style>
