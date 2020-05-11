3a35293722112930884cc2a603deb30307abba01
Alfred-Lau <jian.liu@icloud.com>
2020-05-11 11:30:39 +0800

    chore:hidden meanging page

--- src/components/Menu/menu.vue
+++ src/components/Menu/menu.vue
@@ -1,31 +1,35 @@
 <template>
   <div class="menu">
     <ul class="menu-items">
-      <li class="menu-item"><span><router-link to="/">首页</router-link></span></li>
-      <li class="menu-item"><span><router-link to="/piecework">归档</router-link></span></li>
-      <li class="menu-item"><span><router-link to="/meaning">林泉之秀</router-link></span></li>
-      <li class="menu-item"><span><router-link to="/about">关于我</router-link></span></li>
+      <li class="menu-item">
+        <span><router-link to="/">首页</router-link></span>
+      </li>
+      <li class="menu-item">
+        <span><router-link to="/piecework">归档</router-link></span>
+      </li>
+      <!-- <li class="menu-item"><span><router-link to="/meaning">林泉之秀</router-link></span></li> -->
+      <li class="menu-item">
+        <span><router-link to="/about">关于我</router-link></span>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
-export default {
-
-};
+export default {};
 </script>
 
 <style lang="scss" scoped>
-
 .menu {
-    font-family: BlinkMacSystemFont,Segoe UI,PingFang SC,Hiragino Sans GB,Microsoft YaHei,Helvetica Neue,Helvetica,Arial,sans-serif;
-    color: rgba($color: #4a4a4a, $alpha: .65);
-  &-items{
+  font-family: BlinkMacSystemFont, Segoe UI, PingFang SC, Hiragino Sans GB,
+    Microsoft YaHei, Helvetica Neue, Helvetica, Arial, sans-serif;
+  color: rgba($color: #4a4a4a, $alpha: 0.65);
+  &-items {
     display: flex;
     flex-direction: row;
   }
 
-  &-item{
+  &-item {
     color: #4a4a4a;
     list-style: none;
 
@@ -34,11 +38,10 @@ export default {
       padding: 0 20px;
       display: inline-block;
       line-height: 20px;
-      a{
-
+      a {
         color: #4a4a4a;
       }
     }
   }
-}
-</style>>
+}</style
+>>
